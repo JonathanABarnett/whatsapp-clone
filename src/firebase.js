bedd1f59538e0c5ff9ee27a01f0265a8f7ed70e8
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-09-10 10:46:26 -0500

    Add config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJo1zGt4YbImTmZ08iMaQQVvROPILhZ9M",
+  authDomain: "whatsapp-mern-fbac8.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-fbac8.firebaseio.com",
+  projectId: "whatsapp-mern-fbac8",
+  storageBucket: "whatsapp-mern-fbac8.appspot.com",
+  messagingSenderId: "815059943698",
+  appId: "1:815059943698:web:a2ed66a1f0545e38bb73cb",
+  measurementId: "G-E9T7LNYL0L",
+};
